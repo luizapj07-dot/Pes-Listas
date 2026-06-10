76cef91470de75eda6f94dacdd870f2bce1f946e
Luiz jr <Luiz.apj07@aluno.ifsc.edu.br>
2026-06-09 23:50:46 -0300

    lista2

---
+++ Lista 2/teste.py
@@ -0,0 +1,12 @@
+# nome = "luiz"
+
+# for letra in nome: 
+#     print(letra)
+
+# for i in range(1,100,2):
+#     print(i)
+
+frutas = ["banana", "maça", "laranja"]
+
+for fruta in frutas:
+    print(fruta)
