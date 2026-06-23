b1e92adab26670f6aedc28e77a75bac78106075a
luizapj07-dot <luiz.apj07>
2026-06-23 17:22:22 -0300

    pronto

---
+++ Lista 3/Exercício 3.py
@@ -0,0 +1,37 @@
+listas = []
+for i in range(10): #definindo como -1
+    valor = "-1"
+    listas.append(valor)
+    contador = 0
+    
+opcao = 3
+while opcao != "0": # inicio
+    print("""
+    Menu
+    ----
+    1 – Cadastrar
+    2 – Listar todos
+    0 – Sair
+    """)
+    
+    opcao = input("Digite sua opcao:")
+
+    if opcao == "1":
+        print("contador => ", contador)
+        while contador <= 10:
+            if listas[contador] == "-1":
+                sabor = int(input("Digite o novo valor:"))
+                if sabor == "-1":
+                    break
+                else:
+                    valor = sabor
+                    contador += 1
+            else:
+                contador += 1
+
+    if opcao == "2":
+        for valor in listas:
+            print(valor)
+    
+    if opcao == "0":
+        break
