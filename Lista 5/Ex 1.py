809c48d6bb98a72ee3069b6fa75dfbdb33abad1c
luiz.apj07-dot <luiz.apj07>
2026-08-19 11:17:20 -0300

    Lista 5 Ex 1

---
+++ Lista 5/Ex 1.py
@@ -0,0 +1,8 @@
+def media(a, b, c):
+    print((a+b+c)/3)
+
+v1 = int(input("Digite um valor para v1: "))
+v2 = int(input("Digite um valor para v2: "))
+v3 = int(input("Digite um valor para v3: "))
+media(v1, v2, v3)
+
