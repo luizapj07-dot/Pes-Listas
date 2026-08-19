e1745cd1d900f3886d77463953b21c6173f10095
luiz.apj07-dot <luiz.apj07>
2026-08-19 11:38:13 -0300

    Ex 4

---
+++ Lista 5/Ex 4.py
@@ -0,0 +1,10 @@
+def soma(a, b, c, d):
+    res = a+b+c+d
+    return res
+
+v1 = int(input("Digite o v1: "))
+v2 = int(input("Digite o v2: "))
+v3 = int(input("Digite o v3: "))
+v4 = int(input("Digite o v4: "))
+
+print(soma(v1, v2, v3, v4))
