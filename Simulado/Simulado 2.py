63a2dba261cfbf62f41eff11c58a94815f04019f
claustrum0413 <eduardoschuquelmattioni>
2026-08-25 16:28:50 -0300

    simulado 2 feito

---
+++ Simulado/Simulado 2.py
@@ -0,0 +1,12 @@
+lista = []
+
+while len(lista) <= 15:
+    num = int(input("Digite um numero: "))
+    if num < 1 or num > 75:
+        print("Outro né")
+    else:
+        print("Número registrado com sucesso!! ")
+        lista.append(num)
+
+lista.sort() #Vai ordenar do menor pro maior
+print(lista)
