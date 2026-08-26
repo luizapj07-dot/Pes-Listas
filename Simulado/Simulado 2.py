ac6efe87c6251dbae52e5c05a35bac48e560438f
luiz.apj07 <luiz.apj07@aluno.ifsc.edu.br>
2026-08-26 11:36:52 -0300

    Simulado Feito

--- Simulado/Simulado 2.py
+++ Simulado/Simulado 2.py
@@ -2,7 +2,7 @@ lista = []
 
 while len(lista) <= 15:
     num = int(input("Digite um numero: "))
-    if num < 1 or num > 75:
+    if (num < 1 or num > 75) and num not in lista:
         print("Outro né")
     else:
         print("Número registrado com sucesso!! ")
