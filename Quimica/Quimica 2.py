fe76a71af28eae2786d5dbaa41c768eeb0ea6e3c
lyag-dev <lyagushkatribola@gmail.com>
2026-08-27 09:02:45 -0300

    2

---
+++ Quimica/Quimica 2.py
@@ -0,0 +1,8 @@
+
+massa_soluto = int(input("Qual a massa do suluto (em gramas): "))
+volume_solucao = float(input("Qual o volume da solução (em litros): "))
+
+concentracao = massa_soluto/volume_solucao
+
+print(f"{concentracao} g/L")
+                           
